98655dcd6b118d3ad3bd85da8f42f44c889783bb
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-07-26 09:34:05 -0300

    :fire: Creando seeders

---
+++ database/seeders/CurrenciesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Currency;
+use Illuminate\Database\Seeder;
+
+class CurrenciesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currencies = [
+            'usd',
+            'eur',
+            'gbp',
+            'ars'
+        ];
+
+        foreach ($currencies as $currency){
+            Currency::create([
+                'iso' => $currency
+            ]);
+        }
+
+    }
+}
---
+++ database/seeders/PaymentPlatformsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PaymentPlatform;
+use Illuminate\Database\Seeder;
+
+class PaymentPlatformsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        PaymentPlatform::create([
+            'name' => 'PayPal',
+            'image' => 'img/payment-platforms/paypal.jpg'
+        ]);
+        PaymentPlatform::create([
+            'name' => 'Stripe',
+            'image' => 'img/payment-platforms/stripe.jpg'
+        ]);
+    }
+}
