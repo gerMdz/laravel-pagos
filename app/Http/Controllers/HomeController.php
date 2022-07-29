34d23274f422fe586e5c355fd34e8b8e470161c0
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-07-29 08:20:42 -0300

    :sparkles: Modificando el form de moneda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Currency;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $currencies = Currency::all();
+        return view('home')->with([
+            'currencies' => $currencies,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        <form action="#" method="POST" id="paymentForm">
+                            @csrf
+                            <div class="row">
+                                <div class="col-auto">
+                                    <label for="payMont"> ¿Cuanto desea pagar?</label>
+                                    <input
+                                        id="payMont"
+                                        type="number"
+                                        min="5"
+                                        step="0.01"
+                                        class="form-control"
+                                        value="{{ mt_rand(500, 10000) /100 }}"
+                                        required
+                                    >
+                                    <small class="form-text text-muted">
+                                        Use valores decimales separados por puntos
+                                    </small>
 
-                    {{ __('You are logged in!') }}
+                                </div>
+                                <div class="col-auto">
+                                    <label for="payCurrency">Moneda</label>
+                                    <select name="currency" required id="payCurrency" class="form-select">
+                                        @foreach($currencies  as $currency)
+                                            <option value="{{$currency->iso}}">
+                                                {{strtoupper($currency->iso)}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="text-center mt-3">
+                                    <button type="submit" id="payButton"
+                                            class="btn btn-primary btn-lg"
+                                    >
+                                        Pagar
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
