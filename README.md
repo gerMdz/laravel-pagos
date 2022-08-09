9b2f175f26cfbe2973ff62d061684e3cc0d2e25b
gerMDZ Gerardo Montivero <gerardo.montivero@gmail.com>
2022-08-09 08:28:04 -0300

    :sparkles: Aplicando la API de PayPal y creando una orden

--- README.md
+++ README.md
@@ -21,6 +21,12 @@ Laravel is a web application framework with expressive, elegant syntax. We belie
 
 Laravel is accessible, powerful, and provides tools required for large, robust applications.
 
+## Tips Laravel
+Documentación de PayPal
+> https://developer.paypal.com/docs/api/orders/v2/#orders_create
+
+
+
 ## Learning Laravel
 
 Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Traits\ConsumesExternalServices;
+use GuzzleHttp\Exception\GuzzleException;
 
 class PayPalService
 {
@@ -14,18 +15,56 @@ class PayPalService
 
     public function __construct()
     {
-     $this->baseUri = config('service.paypal.base_uri');
-     $this->clientId = config('service.paypal.client_id');
-     $this->clientSecret = config('service.paypal.client_secret');
+        $this->baseUri = config('service.paypal.base_uri');
+        $this->clientId = config('service.paypal.client_id');
+        $this->clientSecret = config('service.paypal.client_secret');
     }
 
     public function resolveAuthorization(&$queryParams, &$formsParams, &$headers)
     {
-
+        $headers['Authorization'] = $this->resolveAccessToken();
     }
 
     public function decodeResponse($response)
     {
+        return json_decode($response);
+    }
 
+    public function resolveAccessToken(): string
+    {
+        $credentials = base64_encode("{$this->clientId}:{$this->clientSecret}");
+        return "Basic {$credentials}";
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function createOrder($value, $currency): string
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v2/checkout/orders',
+            [],
+            [
+                'intent' => 'CAPTURE',
+                "purchase_units" => [
+                    0 => [
+                        'amount' => [
+                            'currency_code' => strtoupper($currency),
+                            'value' => $value
+                        ],
+                    ],
+                ],
+                'application_context' => [
+                    'brand_name' => config('app.name'),
+                    'shipping_preference' => 'NO_SHIPPING',
+                    'user_action' => 'PAY_NOW',
+                    'return_url' => route('approval'),
+                    'cancel_url' => route('cancelled'),
+                ],
+            ],
+            [],
+            true
+        );
     }
 }
