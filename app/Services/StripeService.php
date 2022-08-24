d51ec4a28934bd2c97a3d42eb0d890c63ad58800
gerardo montivero <gerardo.montivero@gmail.com>
2022-08-24 12:04:09 -0300

    :heavy_minus_sign: Procesamiento de pagos stripe

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -2,27 +2,30 @@
 
 namespace App\Services;
 
-use App\Traits\ConsumesExternalServices;
-use GuzzleHttp\Exception\GuzzleException;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Traits\ConsumesExternalServices;
 
 class StripeService
 {
     use ConsumesExternalServices;
 
-    protected $baseUri;
     protected $key;
+
     protected $secret;
+
+    protected $baseUri;
+
+    protected $plans;
 
     public function __construct()
     {
+        $this->baseUri = config('services.stripe.base_uri');
         $this->key = config('services.stripe.key');
         $this->secret = config('services.stripe.secret');
-        $this->baseUri = config('services.stripe.base_uri');
+        $this->plans = config('services.stripe.plans');
     }
 
-    public function resolveAuthorization(&$queryParams, &$formsParams, &$headers)
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
     {
         $headers['Authorization'] = $this->resolveAccessToken();
     }
@@ -32,50 +35,125 @@ class StripeService
         return json_decode($response);
     }
 
-    public function resolveAccessToken(): string
+    public function resolveAccessToken()
     {
         return "Bearer {$this->secret}";
     }
 
-    /**
-     * @throws GuzzleException
-     */
     public function handlePayment(Request $request)
     {
-//        $order = $this->createOrder($request->value, $request->currency);
-//
-//        $orderLinks = collect($order->links);
-//
-//        $approve = $orderLinks->where('rel', 'approve')->first();
-//
-//        session()->put('approvalId', $order->id);
-//
-//        return redirect($approve->href);
+        $request->validate([
+            'payment_method' => 'required',
+        ]);
+
+        $intent = $this->createIntent($request->value, $request->currency, $request->payment_method);
+
+        session()->put('paymentIntentId', $intent->id);
+
+        return redirect()->route('approval');
     }
-
-
-
-
-
-
 
     public function handleApproval()
     {
-//
+        if (session()->has('paymentIntentId')) {
+            $paymentIntentId = session()->get('paymentIntentId');
+
+            $confirmation = $this->confirmPayment($paymentIntentId);
+
+            if ($confirmation->status === 'requires_action') {
+                $clientSecret = $confirmation->client_secret;
+
+                return view('stripe.3d-secure')->with([
+                    'clientSecret' => $clientSecret,
+                ]);
+            }
+
+            if ($confirmation->status === 'succeeded') {
+                $name = $confirmation->charges->data[0]->billing_details->name;
+                $currency = strtoupper($confirmation->currency);
+                $amount = $confirmation->amount / $this->resolveFactor($currency);
+
+                return redirect()
+                    ->route('home')
+                    ->withSuccess(['payment' => "Thanks, {$name}. We received your {$amount}{$currency} payment."]);
+            }
+        }
+
+        return redirect()
+            ->route('home')
+            ->withErrors('We were unable to confirm your payment. Try again, please');
+    }
+
+    public function handleSubscription(Request $request)
+    {
+        $customer = $this->createCustomer(
+            $request->user()->name,
+            $request->user()->email,
+            $request->payment_method
+        );
+
+        $subscription = $this->createSubscription(
+            $customer->id,
+            $request->payment_method,
+            $this->plans[$request->plan]
+        );
+
+        if ($subscription->status == 'active') {
+            session()->put('subscriptionId', $subscription->id);
+
+            return redirect()->route(
+                'subscribe.approval',
+                [
+                    'plan' => $request->plan,
+                    'subscription_id' => $subscription->id,
+                ],
+            );
+        }
+
+        $paymentIntent = $subscription->latest_invoice->payment_intent;
+
+        if ($paymentIntent->status === 'requires_action') {
+            $clientSecret = $paymentIntent->client_secret;
+
+            session()->put('subscriptionId', $subscription->id);
+
+            return view('stripe.3d-secure-subscription')->with([
+                'clientSecret' => $clientSecret,
+                'plan' => $request->plan,
+                'paymentMethod' => $request->payment_method,
+                'subscriptionId' => $subscription->id,
+            ]);
+        }
+
+        return redirect()->route('subscribe.show')
+            ->withErrors('We were unable to activate your subscription. Try again, please.');
+    }
+
+    public function validateSubscription(Request $request)
+    {
+        if (session()->has('subscriptionId')) {
+            $subscriptionId = session()->get('subscriptionId');
+
+            session()->forget('subscriptionId');
+
+            return $request->subscription_id == $subscriptionId;
+        }
+
+        return false;
     }
 
     public function createIntent($value, $currency, $paymentMethod)
     {
-       return $this->makeRequest(
+        return $this->makeRequest(
             'POST',
             '/v1/payment_intents',
             [],
             [
                 'amount' => round($value * $this->resolveFactor($currency)),
-                'currency' => strtolower($currency) ,
+                'currency' => strtolower($currency),
                 'payment_method' => $paymentMethod,
-                'confirmation_method' => 'manual'
-            ]
+                'confirmation_method' => 'manual',
+            ],
         );
     }
 
@@ -84,19 +162,48 @@ class StripeService
         return $this->makeRequest(
             'POST',
             "/v1/payment_intents/{$paymentIntentId}/confirm",
-            [],
-            []
         );
     }
 
+    public function createCustomer($name, $email, $paymentMethod)
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v1/customers',
+            [],
+            [
+                'name' => $name,
+                'email' => $email,
+                'payment_method' => $paymentMethod,
+            ],
+        );
+    }
 
+    public function createSubscription($customerId, $paymentMethod, $priceId)
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v1/subscriptions',
+            [],
+            [
+                'customer' => $customerId,
+                'items' => [
+                    ['price' => $priceId],
+                ],
+                'default_payment_method' => $paymentMethod,
+                'expand' => ['latest_invoice.payment_intent']
+            ],
+        );
+    }
 
-    public function resolveFactor($currency): int
+    public function resolveFactor($currency)
     {
         $zeroDecimalCurrencies = ['JPY'];
-        if(in_array(strtoupper($currency), $zeroDecimalCurrencies)){
-            return 100;
+
+        if (in_array(strtoupper($currency), $zeroDecimalCurrencies)) {
+            return 1;
         }
-        return 1;
+
+        return 100;
     }
 }
